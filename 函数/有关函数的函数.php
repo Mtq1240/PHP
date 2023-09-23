5d8da9bcd77b1d136ae7b3402215cb84189da3c7
Mtq1240 <119779903+Mtq1240@users.noreply.github.com>
2023-09-23 16:51:33 +0800

    Create 有关函数的函数.php

---
+++ 函数/有关函数的函数.php
@@ -0,0 +1,14 @@
+<?php
+
+    //函数相关函数
+    function test($a,$b) {
+        //获取指定参数
+        var_dump(func_get_arg(1));
+        //获取所有参数
+        var_dump(func_get_args());
+        //获取参数数量
+        var_dump(func_num_args());
+    }
+
+    //调用函数
+    function_exists('test') && test(1,'2',3,4);
